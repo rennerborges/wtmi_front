8867c161e5e53c61cf80efed4e923d933dc767a7
Renner Borges <rennerferreira23@gmail.com>
2022-06-07 12:34:22 -0300

    chore: react-router

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import RegisterPresence from './pages/RegisterPresence';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <RegisterPresence />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Em breve </h1>} />
+          <Route path="/scheduler/:id" element={<RegisterPresence />} />
+          <Route path="*" element={<h1>Not found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
